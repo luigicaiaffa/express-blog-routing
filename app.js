956af1f3e1633dbac6ca347ba3454ad72ad61a33
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-20 15:23:58 +0100

    add index

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-// # Array di post
-const path = require("path");
-const postsPath = path.join(__dirname, "db", "postslist");
-const posts = require(postsPath);
-
 // # Rotte
-app.get("/", (req, res) => {
-  res.json("Server del mio blog");
-});
+const pagesRouter = require("./routers/pages");
+const postsRouter = require("./routers/posts");
 
-app.get("/bacheca", (req, res) => {
-  res.json({ posts });
-});
+app.use("/", pagesRouter);
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`"Server del mio blog" listening on port ${port}`);
--- routers/posts.js
+++ routers/posts.js
@@ -2,8 +2,15 @@
 const express = require("express");
 const router = express.Router();
 
+// # Array di post
+const posts = require("../db/postslist.js");
+
 // # Rotte
 // index
+router.get("/", (req, res) => {
+  res.json("Lista dei post");
+  // res.json(posts);
+});
 
 // show
 
