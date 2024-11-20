4771425383b0bea3b19708e0f17ea757a0243fe9
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-20 14:59:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// # Configurazione
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+// # Array di post
+const path = require("path");
+const postsPath = path.join(__dirname, "posts");
+const posts = require(postsPath);
+
+// # Rotte
+app.get("/", (req, res) => {
+  res.json("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json({ posts });
+});
+
+app.listen(port, () => {
+  console.log(`"Server del mio blog" listening on port ${port}`);
+});
---
+++ posts.js
@@ -0,0 +1,35 @@
+// # Array di Post
+const blogPosts = [
+  {
+    title: "Ciambellone",
+    content: "ciambellone classico",
+    img: "/img/ciambellone.jpeg",
+    tags: ["#dolce", "#farina", "#ciambellone", "#cibo"],
+  },
+  {
+    title: "Cracker alla Barbabietola",
+    content: "cracker alla barbabietola ai 5 semi",
+    img: "/img/cracker_barbabietola.jpeg",
+    tags: ["#snack", "#salato", "#barbabietola", "#cracker", "#cibo"],
+  },
+  {
+    title: "Pane Fritto Dolce",
+    content: "pane fritto con aggiunta di zucchero",
+    img: "/img/pane_fritto_dolce.jpeg",
+    tags: ["#dolce", "#pane", "#fritto", "#cibo"],
+  },
+  {
+    title: "Rigatoni alla barbabietola",
+    content: "rigatoni con crema di barbabietola e granella di pistacchi",
+    img: "/img/pasta_barbabietola.jpeg",
+    tags: ["#pasta", "#barbabietola", "#Cibo"],
+  },
+  {
+    title: "Torta Paesana",
+    content: "torta al cioccolato con mandorle",
+    img: "/img/torta_paesana.jpeg",
+    tags: ["#torta", "#dolce", "#mandorle", "#cioccolato", "#Cibo"],
+  },
+];
+
+module.exports = blogPosts;
